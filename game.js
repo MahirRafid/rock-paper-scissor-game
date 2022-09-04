8229b1464bfdcfe1587b0f7101cf9a4f6e99d608
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-09-04 21:21:04 +0600

    Added DOM and js for calculation

--- game.js
+++ game.js
@@ -7,63 +7,84 @@ function getComputerChoice() {
 
 function playRound(userChoice, computerChoice) {
   userChoice = userChoice.toUpperCase();
+  let result = document.querySelector(".result");
+  let desc = document.querySelector(".desc");
   if (userChoice == "ROCK") {
     if (computerChoice == "ROCK") {
-      console.log("TIE. Please try again.");
+      result.textContent = "It's TIE!";
+      desc.textContent = "Rock ties with rock";
       return 0;
     } else if (computerChoice == "PAPER") {
-      console.log("You LOSE. Paper beats rock.");
+      result.textContent = "You Lose :(";
+      desc.textContent = "Paper beats Rock";
       return "lose";
     } else {
-      console.log("You WIN. Rock beats Scissors.");
+      result.textContent = "You Win :)";
+      desc.textContent = "Rock beats Scissors";
       return "win";
     }
   } else if (userChoice == "PAPER") {
     if (computerChoice == "PAPER") {
-      console.log("TIE. Please try again.");
+      result.textContent = "It's TIE!";
+      desc.textContent = "Rock ties with rock";
       return 0;
     } else if (computerChoice == "ROCK") {
-      console.log("You WIN. Paper beats rock.");
+      result.textContent = "You Win :)";
+      desc.textContent = "Paper beats rock";
       return "win";
     } else {
-      console.log("You LOSE. Scissors beat paper");
+      result.textContent = "You Lose :(";
+      desc.textContent = "Scissors beat paper";
       return "lose";
     }
   } else if (userChoice == "SCISSORS") {
     if (computerChoice == "SCISSORS") {
-      console.log("TIE. Please try again.");
+      result.textContent = "It's TIE!";
+      desc.textContent = "Scissors tie with scissors";
       return 0;
     } else if (computerChoice == "ROCK") {
-      console.log("You LOSE. Rock beats scissors.");
+      result.textContent = "You Lose :(";
+      desc.textContent = "Rock beats Scissors";
       return "lose";
     } else {
-      console.log("You WIN. Paper beat scissors");
+      result.textContent = "You Win :)";
+      desc.textContent = "Scissors beat paper";
       return "win";
     }
-  } else {
-    alert("Please check your spelling.");
-    return "spellError";
   }
 }
 
 function game() {
   let userScr = 0,
     cmpScr = 0;
-  for (let i = 0; i < 5; i++) {
-    let userChoice = prompt("Rock, paper or Scissors ? :");
-    let computerChoice = getComputerChoice();
-    let ans = playRound(userChoice, computerChoice);
+  let rock = document.querySelector(".rock");
+  let paper = document.querySelector(".paper");
+  let scissors = document.querySelector(".scissor");
+
+  rock.addEventListener("click", () => {
+    let ans = playRound("rock", getComputerChoice());
     if (ans == "win") userScr++;
     else if (ans == "lose") cmpScr++;
-    else if (ans == "spellError") i--;
-  }
+  });
 
-  if (userScr > cmpScr) {
-    console.log("WOHOO!! YOU WON!!!");
-  } else if (cmpScr > userScr) {
-    console.log("SORRY! YOU LOST :(");
-  } else {
-    console.log("MATCH TIE. PLAY AGAIN.");
+  paper.addEventListener("click", () => {
+    let ans = playRound("rock", getComputerChoice());
+    if (ans == "win") userScr++;
+    else if (ans == "lose") cmpScr++;
+  });
+
+  scissors.addEventListener("click", () => {
+    let ans = playRound("rock", getComputerChoice());
+    if (ans == "win") userScr++;
+    else if (ans == "lose") cmpScr++;
+  });
+
+  if (userScr == 5) {
+    alert("WOHOO!! YOU WON!!!");
+    return;
+  } else if (cmpScr == 5) {
+    alert("SORRY! YOU LOST :(");
+    return;
   }
 }
 
