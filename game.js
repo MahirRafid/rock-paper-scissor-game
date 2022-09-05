40e57b92188b0d0894e0396dc9523aa15fef8841
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-09-05 10:51:43 +0600

    Added js to add hand gestures in the scorecard

--- game.js
+++ game.js
@@ -9,44 +9,64 @@ function playRound(userChoice, computerChoice) {
   userChoice = userChoice.toUpperCase();
   let result = document.querySelector(".result");
   let desc = document.querySelector(".desc");
+  let userImg = document.querySelector(".userImg");
+  let cmpImg = document.querySelector(".cmpImg");
   if (userChoice == "ROCK") {
     if (computerChoice == "ROCK") {
+      userImg.innerHTML = "<img src='images/rock.png'>";
+      cmpImg.innerHTML = "<img src='images/rock.png'>";
       result.textContent = "It's TIE!";
       desc.textContent = "Rock ties with rock";
       return 0;
     } else if (computerChoice == "PAPER") {
+      userImg.innerHTML = "<img src='images/rock.png'>";
+      cmpImg.innerHTML = "<img src='images/paper.png'>";
       result.textContent = "You Lose :(";
       desc.textContent = "Paper beats Rock";
       return "lose";
     } else {
+      userImg.innerHTML = "<img src='images/rock.png'>";
+      cmpImg.innerHTML = "<img src='images/scissors.png'>";
       result.textContent = "You Win :)";
       desc.textContent = "Rock beats Scissors";
       return "win";
     }
   } else if (userChoice == "PAPER") {
     if (computerChoice == "PAPER") {
+      userImg.innerHTML = "<img src='images/paper.png'>";
+      cmpImg.innerHTML = "<img src='images/paper.png'>";
       result.textContent = "It's TIE!";
       desc.textContent = "Rock ties with rock";
       return 0;
     } else if (computerChoice == "ROCK") {
+      userImg.innerHTML = "<img src='images/paper.png'>";
+      cmpImg.innerHTML = "<img src='images/rock.png'>";
       result.textContent = "You Win :)";
       desc.textContent = "Paper beats rock";
       return "win";
     } else {
+      userImg.innerHTML = "<img src='images/paper.png'>";
+      cmpImg.innerHTML = "<img src='images/scissors.png'>";
       result.textContent = "You Lose :(";
       desc.textContent = "Scissors beat paper";
       return "lose";
     }
   } else if (userChoice == "SCISSORS") {
     if (computerChoice == "SCISSORS") {
+      userImg.innerHTML = "<img src='images/scissors.png'>";
+      cmpImg.innerHTML = "<img src='images/scissors.png'>";
       result.textContent = "It's TIE!";
       desc.textContent = "Scissors tie with scissors";
       return 0;
     } else if (computerChoice == "ROCK") {
+      userImg.innerHTML = "<img src='images/scissors.png'>";
+      cmpImg.innerHTML = "<img src='images/rock.png'>";
       result.textContent = "You Lose :(";
       desc.textContent = "Rock beats Scissors";
       return "lose";
     } else {
+      userImg.innerHTML = "<img src='images/scissors.png'>";
+      cmpImg.innerHTML = "<img src='images/paper.png'>";
       result.textContent = "You Win :)";
       desc.textContent = "Scissors beat paper";
       return "win";
@@ -58,14 +78,14 @@ let userScr = 0,
   cmpScr = 0;
 
 function updateScore() {
-  let ans = playRound("rock", getComputerChoice());
   let userScrBtn = document.querySelector(".userScr");
   let cmpScrBtn = document.querySelector(".cmpScr");
+  let ans = playRound("rock", getComputerChoice());
   if (ans == "win") {
-    userScr++;
+    ++userScr;
     userScrBtn.textContent = `${userScr}`;
   } else if (ans == "lose") {
-    cmpScr++;
+    ++cmpScr;
     cmpScrBtn.textContent = `${cmpScr}`;
   }
 
